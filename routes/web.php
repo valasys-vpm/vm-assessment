6eef5f5a3d1782ab7772fe4227c0d571d4a65534
valasys-vpm <87777274+valasys-vpm@users.noreply.github.com>
2021-09-24 18:23:04 +0530

    index routed updated

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    Route::view('/login', 'login')->name('login');
+    return redirect()->route('login');
 });
 
 Auth::routes();
