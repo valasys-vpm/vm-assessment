06ed65dc604ca460244732717c5e75f08c93879a
valasys-vpm <87777274+valasys-vpm@users.noreply.github.com>
2021-09-24 18:33:24 +0530

    status parse to int js changes done

--- public/js/admin/assessment.js
+++ public/js/admin/assessment.js
@@ -31,7 +31,7 @@ $(function (){
             },
             {
                 render: function (data, type, row) {
-                    switch (row.status) {
+                    switch (parseInt(row.status)) {
                         case 1: return '<span class="badge badge-pill badge-info" style="padding: 5px;min-width:50px;">Active</span>';
                         case 0: return '<span class="badge badge-pill badge-danger" style="padding: 5px;min-width:50px;">Inactive</span>';
                         case 2: return '<span class="badge badge-pill badge-success" style="padding: 5px;min-width:50px;">Completed</span>';
