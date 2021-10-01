5c56f2a5b59e057bf2e446ae443d7feb4c62eb09
valasys-vpm <87777274+valasys-vpm@users.noreply.github.com>
2021-10-01 18:32:01 +0530

    assessment submit bug resolutions

--- app/Http/Controllers/User/AssessmentController.php
+++ app/Http/Controllers/User/AssessmentController.php
@@ -123,7 +123,7 @@ class AssessmentController extends Controller
         try {
             DB::beginTransaction();
             $userAssessment = UserAssessment::findOrFail(base64_decode($attributes['user_assessment_id']));
-            if(!empty($userAssessment->answer_given) && $userAssessment->attempted > 0) {
+            if(empty($userAssessment->answer_given) && $userAssessment->attempted == 0) {
                 $userAssessment->answer_given = json_encode($attributes['answer']);
                 $userAssessment->attempted = count($attributes['answer']);
                 $marks = 0;
@@ -134,8 +134,9 @@ class AssessmentController extends Controller
                     }
                 }
                 $userAssessment->marks_obtained = $marks;
-            } else {
                 $userAssessment->rank = 1;
+            } else {
+                $userAssessment->rank = $userAssessment->rank + 1;
             }
             $userAssessment->save();
             if($userAssessment->id) {
