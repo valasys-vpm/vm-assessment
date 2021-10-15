08cbb266d8b46aecc5684af68d02f1b18b3dd877
valasys-vpm <87777274+valasys-vpm@users.noreply.github.com>
2021-10-16 00:24:44 +0530

    temp changes for result

--- app/Http/Controllers/Admin/AssessmentController.php
+++ app/Http/Controllers/Admin/AssessmentController.php
@@ -238,10 +238,10 @@ class AssessmentController extends Controller
             $query = User::query();
             $query->whereRoleId(2);
 
-            //$query->whereDepartmentId(2);
+            $query->whereDepartmentId(2);
             //$query->whereDesignationId(2);
             //$query->whereDesignationId(17);
-            $query->whereIn('department_id', [4,5]);
+            //$query->whereIn('department_id', [4,5]);
 
             $query->whereNotIn('employee_code', ['VBS034']);
             $query->orderBy('employee_code');
